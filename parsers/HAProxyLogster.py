eac1cf83ce40982bdf46710b397c4140e88ae7b3
Seb Bacon <seb.bacon@gmail.com>
2014-04-22 10:33:44 +0100

    Collect www and api stats separately

--- parsers/HAProxyLogster.py
+++ parsers/HAProxyLogster.py
@@ -31,30 +31,53 @@ class HAProxyLogster(LogsterParser):
         '''Initialize any data structures or variables needed for keeping track
         of the tasty bits we find in the log we are parsing.'''
         self.unblocks = 0
-        self.time = 0
-        self.count = 0.0
-        self.regex = re.compile('.*?\d+/\d+/\d+/\d+/(\d+).* (\S+) HTTP.*')
+        self.all_time = 0
+        self.all_count = 0.0
+        self.www_count = 0.0
+        self.www_time = 0
+        self.api_count = 0.0
+        self.api_time = 0
+        self.regex = re.compile('.*? ([^ ]+) \d+/\d+/\d+/\d+/(\d+).* (\S+) HTTP.*')
 
     def parse_line(self, line):
         try:
             # Apply regular expression to each line and extract
             # interesting bits.
-            time, url = self.regex.match(line).groups()
-            self.time += int(time)
-            self.count += 1
+            backend, time, url = self.regex.match(line).groups()
+            self.all_time += int(time)
+            self.all_count += 1
             if url == "/unblock_requests/new":
                 self.unblocks += 1
+            if backend.startswith("www"):
+                self.www_count += 1
+                self.www_time += int(time)
+            elif backend.startswith("api"):
+                self.api_count += 1
+                self.api_time += int(time)
         except Exception, e:
             raise LogsterParsingException, "regmatch or contents failed with %s" % e
 
 
     def get_state(self, duration):
         # Return a list of metrics objects
-        if self.count:
-            timePerReq = self.time/self.count
+        if self.all_count:
+            all_timePerReq = self.all_time/self.all_count
         else:
-            timePerReq = 0
+            all_timePerReq = 0
+        if self.www_count:
+            www_timePerReq = self.www_time/self.www_count
+        else:
+            www_timePerReq = 0
+        if self.api_count:
+            api_timePerReq = self.api_time/self.api_count
+        else:
+            api_timePerReq = 0
         return [
             MetricObject("logs.haproxy.blocks", self.unblocks, metric_type="c"),
-            MetricObject("logs.haproxy.responsetime", str(timePerReq), metric_type="ms")
+            MetricObject("logs.haproxy.requests", self.all_count, metric_type="c"),
+            MetricObject("logs.haproxy.www.requests", self.www_count, metric_type="c"),
+            MetricObject("logs.haproxy.api.requests", self.api_count, metric_type="c"),
+            MetricObject("logs.haproxy.responsetime", str(all_timePerReq), metric_type="ms"),
+            MetricObject("logs.haproxy.www.responsetime", str(www_timePerReq), metric_type="ms"),
+            MetricObject("logs.haproxy.api.responsetime", str(api_timePerReq), metric_type="ms")
         ]
